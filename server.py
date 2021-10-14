ea5fe158691ff7807196db4a882500355939fccb
zgong26 <79713887+zgong26@users.noreply.github.com>
2021-10-14 11:15:56 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -2,15 +2,15 @@ import sys
 import random
 from socket import *
 
-#dictionary for registered clients
+#dictonary for registered clients
 userDict = {}
 DHTList = []
 #if DHT has been setup
 DHT_setup = False
-#if DHT is under settingup
+#if DHT is under setting up
 ifSettingup = False
 
-print("Port number: ",end="")
+print("Port number: ", end = "")
 port = int(input())
 #socket used to bind the port
 sSocket = socket(AF_INET, SOCK_DGRAM)
@@ -18,70 +18,62 @@ sSocket.bind(('', port))
 print("Server is now receiving from port", port)
 
 while True:
-	oriMessage, cAddress = sSocket.recvfrom(2048)
-	print("received", cAddress)
+    oriMessage, cAddress = sSocket.recvfrom(2048)
     #decode messages
-	message = oriMessage.decode()
-	command = message.split(" ")[0]
+    message = oriMessage.decode()
+    command = message.split(" ")[0]
 
-	if command == "register":
-		name = message.split(" ")[1]
-		IPv4 = message.split(" ")[2]
-		port = message.split(" ")[3]
-        #check if name exists and if same ip and port exist
-		if name not in userDict:
-			for key in userDict:
-				if userDict[key][0] == IPv4 and userDict[key][1] == port:
-					sSocket.sendto("FAILURE".encode(), cAddress)
-				else:
-					userDict[name] = [IPv4, port, "Free"]
-					sSocket.sendto("SUCCESS".encode(), cAddress)
+    if command == "register":
+        name = message.split(" ")[1]
+        IPv4 = message.split(" ")[2]
+        port = message.split(" ")[3]
+        #check if name exists and if same ip and port exists
+        if name not in userDict.keys():
+            portNotOverlap = True
+            for key in userDict:
+                if userDict[key][0] == IPv4 and userDict[key][1] == port:
+                    sSocket.sendto("FAILURE".encode(), cAddress)
+                    portNotOverlap = False
+                    break
+            if portNotOverlap:
+                userDict[name] = [IPv4, port, "Free"]
+                sSocket.sendto("SUCCESS".encode(), cAddress)
+        else:
+            sSocket.sendto("FAILURE".encode(), cAddress)
 
-		else:
-			sSocket.sendto("FAILURE".encode(), cAddress)
-		
-	if command == "deregister":
-		name = message.split(" ")[1]
-        #check if the name exists and the status of the client
-		if userDict.has_key(name):
-			if userDict[name][2] == "Free":
-				del userDict[name]
-				sSocket.sendto("SUCCESS".encode(), cAddress)
-			else:
-				sSocket.sendto("FAILURE".encode(), cAddress)
-		else:
-			sSocket.sendto("FAILURE".encode(), cAddress)
-            
-	if command == "setup-dht":
-		n = message.split(" ")[1]
-		name = message.split(" ")[2]
-		if DHT_setup == True or n < 2 or not userDict.has_key(name) or n > len(userDict):
-			sSocket.sendto("FAILURE".encode(), cAddress)
-		else:
-			userDict[name][2] = "Leader"
+    if command == "deregister":
+        name = message.split(" ")[1]
+
+    if command == "setup-dht":
+        n = message.split(" ")[1]
+        name = message.split(" ")[2]
+        if DHT_setup == True or n < 2 or not userDict.has_key(name) or n > len(userDict):
+            sSocket.sendto("FAILURE".encode(), cAddress)
+        else:
+            userDict[name][2] = "Leader"
             #append the leader to the first part of DHT
-			DHTList.append(name, userDict[name][0], userDict[name][1])
-			tempDict = userDict.copy()
-			del tempDict[name]
-			list(tempDict.keys())
-			selectedName = random.sample(list, k=(n-1))
-			for w in selectedName:
-				userDict[w][2] = "InDHT"
-				DHTList.append(w, userDict[w][0], userDict[w][1])
-			DHT_setup = True
-			sSocket.sendto("SUCCESS".encode(), cAddress)	
-	if command == "dht-complete":
-		name = message.split(" ")[1]
-		if(userDict[name][2] != "Leader"):
-			sSocket.sendto("FAILURE".encode(), cAddress)
-		else:
-			sSocket.sendto("SUCCESS".encode(), cAddress)
+            DHTList.append(name, userDict[name][0], userDict[name][1])
+            tempDict = userDict.copy()
+            del tempDict[name]
+            list(tempDict.keys())
+            selectedName = random.sample(list, k=(n-1))
+            for w in selectedName:
+                userDict[w][2] = "InDHT"
+                DHTList.append(w, userDict[w][0], userDict[w][1])
+            DHT_setup = True
+            sSocket.sendto("SUCCESS".encode(), cAddress)
 
-	if command == "query-dht":
-		name = message.split(" ")[1]
+    if command == "query-dht":
+        name = message.split(" ")[1]
         #check if DHT has already been setup, name exists and the status
-		if(DHT_setup == false or name not in userDict or userDict[name][2] != "Free"):
-			sSocket.sendto("FAILURE".encode(), cAddress)
-		else:
-			sSocket.sendto("SUCCESS".encode(), cAddress)
-			sSocket.sendto(DHTList[random.randrange(0, len(DHTList), 1)].encode(), cAddress)
+        if(DHT_setup == False or name not in userDict or userDict[name][2] != "Free"):
+            sSocket.sendto("FAILURE".encode(), cAddress)
+        else:
+            sSocket.sendto("SUCCESS".encode(), cAddress)
+            sSocket.sendto(DHTList[random.randrange(0, len(DHTList), 1)].encode(), cAddress)
+
+    if command == "dht-complete":
+        name = message.split(" ")[1]
+        if(userDict[name][2] != "Leader"):
+            sSocket.sendto("FAILURE".encode(), cAddress)
+        #Check if complete
